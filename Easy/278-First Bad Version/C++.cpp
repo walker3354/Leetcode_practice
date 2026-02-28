ff27b05ca6b923d17bc9755eea389201aed3fa70
walker3354 <walker3354827@gmail.com>
2026-02-28 13:54:50 +0800

    add binary search

---
+++ Easy/278-First Bad Version/C++.cpp
@@ -0,0 +1,28 @@
+
+
+class Solution {
+    public:
+        bool isBadVersion(int version); // already define(bad = return false)
+
+        int firstBadVersion(int n) { // 1~n
+            if (n == 1) return 1;
+            int left = 1;
+            int right = n;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (isBadVersion(mid) == true) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return left;
+        }
+
+        int bad_method(int n) { // 1~n
+            for (int i = 1; i <= n; i++) {
+                if (isBadVersion(i) == false) return i;
+            }
+            return -1;
+        }
+};
