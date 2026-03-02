3e8f473b1a2462ceb46bcef5c1c7c9d07395ba9b
walker3354 <walker3354827@gmail.com>
2026-03-02 19:11:34 +0800

    add queue operation

--- Medium/71-Simplify Path/C++.cpp
+++ Medium/71-Simplify Path/C++.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 using namespace std;
@@ -5,42 +7,22 @@ using namespace std;
 class Solution {
     public:
         string simplifyPath(string path) {
-            // assume the first must be "/"
             vector<string> stack_str;
-            stack_str.push_back(string(1, path[0]));
-            for (int i = 1; i < path.size(); i++) {
-                string& back_str = stack_str.back();
-                string c(1, path[i]);
-                if (back_str == "/" && c == "/") {
-                    if (i == (path.size() - 1)) continue;
-                    continue;
-                } else if (back_str == ".." && c == "/") {
-                    remove_foler_path(stack_str);
-                } else if (back_str == "." && c == "/") {
-                    stack_str.pop_back(); // remove "."
-                    if (i == (path.size() - 1)) stack_str.pop_back();
-                } else if (back_str == "/") {
-                    if (i == (path.size() - 1)) continue;
-                    stack_str.push_back(c);
-                } else if ((c == "/")) {
-                    if (i == (path.size() - 1)) continue;
-                    stack_str.push_back(c);
+            stringstream ss(path);
+            string token;
+            while (getline(ss, token, '/')) {
+                if (token == "" || token == ".") continue;
+                if (token == "..") {
+                    if (!stack_str.empty()) stack_str.pop_back();
                 } else {
-                    back_str += c;
+                    stack_str.push_back(token);
                 }
             }
 
-            string result;
-            for (auto& str : stack_str) result += str;
-            return result;
-        }
-
-        void remove_foler_path(vector<string>& stack_str) { // this assume legal
-            stack_str.pop_back();                           // ".."
-            stack_str.pop_back();                           // "/"
-            if (stack_str.empty()) {
-                stack_str.push_back("/");
+            string result = "";
+            for (auto& toekn : stack_str) {
+                result += ("/" + toekn);
             }
-            stack_str.pop_back(); // folder name
+            return (result.empty() ? "/" : result);
         }
 };
