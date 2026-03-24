496f25f115e67531bab4110b9868b24d76aff1b2
walker3354 <walker3354827@gmail.com>
2026-03-24 23:05:49 +0800

    add some binary question

--- Medium/33-Search in Rotated Sorted Array/C.c
+++ Medium/33-Search in Rotated Sorted Array/C.c
@@ -1,9 +1,16 @@
 int search(int* nums, int numsSize, int target) {
+    if (numsSize <= 0) return -1;
+    if (numsSize == 1) {
+        if (nums[0] == target) return 0;
+        return -1;
+    }
+
     int left = 0;
     int right = numsSize - 1;
     while (left <= right) {
         int mid = left + (right - left) / 2;
         if (nums[mid] == target) return mid;
+
         if (nums[left] <= nums[mid]) {
             if (nums[left] <= target && nums[mid] > target)
                 right = mid - 1;
@@ -16,5 +23,6 @@ int search(int* nums, int numsSize, int target) {
                 right = mid - 1;
         }
     }
+
     return -1;
 }
