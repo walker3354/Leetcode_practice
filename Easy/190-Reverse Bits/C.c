e2168f0a87350dff869aa2d23738cb4e2e07f4bf
walker3354 <walker3354827@gmail.com>
2026-04-26 21:32:03 +0800

    add better reverse bits answer(but hard to understand)

--- Easy/190-Reverse Bits/C.c
+++ Easy/190-Reverse Bits/C.c
@@ -2,11 +2,23 @@ int reverseBits(int n) {
     unsigned int result = 0;
 
     for (int i = 0; i < 32; i++) {
-        int bit = 0;
-        if (((1U << (31 - i)) & n) != 0) {
-            bit = 1;
+        if ((n & (1U << i)) != 0) {
+            result |= (1U << (31 - i));
         }
-        result |= (bit << i);
     }
     return (int)result;
+}
+
+int better_soulation(int n) {
+    // 1 bit exchange
+    // 2 bit exchange
+    // 4 bit exchange
+    // 8 bit exchange
+    // 16bit exchange
+    n = ((n & (0x55555555U)) << 1) | ((n & (0xAAAAAAAAU)) >> 1);
+    n = ((n & (0x33333333U)) << 2) | ((n & (0xCCCCCCCCU)) >> 2);
+    n = ((n & (0x0F0F0F0FU)) << 4) | ((n & (0xF0F0F0F0U)) >> 4);
+    n = ((n & (0x00FF00FFU)) << 8) | ((n & (0xFF00FF00U)) >> 8);
+    n = ((n & (0x0000FFFFU)) << 16) | ((n & (0xFFFF0000U)) >> 16);
+    return n;
 }
