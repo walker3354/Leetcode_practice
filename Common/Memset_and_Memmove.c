c25f5aa4b98aa889d7e2a904a0f4a8cfe5eea5e0
walker3354 <walker3354827@gmail.com>
2026-05-05 18:15:42 +0800

    add atoi c version

--- Common/Memset_and_Memmove.c
+++ Common/Memset_and_Memmove.c
@@ -1,20 +1,17 @@
 #include <stdlib.h>
-
-void* my_memcpy(void* dest, const void* src, size_t n);
-void* my_memmove(void* dest, const void* src, size_t n);
-
 void* my_memmove(void* dest, const void* src, size_t n) {
-    if (dest == NULL || src == NULL) return NULL;
+    if (n == 0 || dest == NULL || src == NULL) return dest;
 
-    const char* c_src = (const char*)src;
     char* c_dest = (char*)dest;
-    if (c_src > c_dest) {
+    const char* c_src = (const char*)src;
+
+    if (c_src >= c_dest) {
         for (int i = 0; i < n; i++) {
             c_dest[i] = c_src[i];
         }
-    } else if (c_dest > c_src) {
-        for (int i = n - 1; i >= 0; i--) {
-            c_dest[i] = c_src[i];
+    } else {
+        for (int i = 0; i < n; i++) {
+            c_dest[n - 1 - i] = c_src[n - 1 - i];
         }
     }
     return dest;
---
+++ Medium/8-String to Integer (atoi)/C.c
@@ -0,0 +1,30 @@
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <string.h>
+
+int myAtoi(char* s) { // skip space + -, others break
+    int s_len = strlen(s);
+    bool sign = true; // true +, false -
+    int result = 0, c_pos = 0;
+
+    while (c_pos < s_len && s[c_pos] == ' ') { // skip space
+        c_pos++;
+    }
+
+    if (s[c_pos] == '-') {
+        sign = false;
+        c_pos++;
+    } else if (s[c_pos] == '+') {
+        c_pos++;
+    }
+
+    while (c_pos < s_len && s[c_pos] >= '0' && s[c_pos] <= '9') {
+        int num = s[c_pos++] - '0';
+        if (result > INT_MAX / 10 || (result == INT_MAX / 10 && num > 7)) {
+            return sign == true ? INT_MAX : INT_MIN;
+        }
+        result = result * 10 + num;
+    }
+    return sign == true ? result : -result;
+}
